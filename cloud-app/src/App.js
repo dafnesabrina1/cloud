50cf8b39b1a06abf3bcd56fae5eb681ff6d86858
Dafne Sabrina Medina Garcia <dafne@Dafnes-MacBook-Pro.local>
2020-04-11 14:23:38 -0500

    Sign In, Sing Up

--- cloud-app/src/App.js
+++ cloud-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './View/Dashboard.js';
+import SignIn from './View/SignIn.js';
+import SignUp from './View/SignUp.js';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <SignUp/>
     </div>
   );
 }

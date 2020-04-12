e9e2ae8fe8fdbd8ab5cb34e05a24c0e63453e688
Dafne Sabrina Medina Garcia <dafne@Dafnes-MacBook-Pro.local>
2020-04-11 23:03:09 -0500

    my Skills Complete

--- cloud-app/src/components/Orders.js
+++ cloud-app/src/components/Orders.js
@@ -8,17 +8,47 @@ import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-import AddShoppingCartIcon from '@material-ui/icons/AddCircle'
+import AddShoppingCartIcon from '@material-ui/icons/AddCircle';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
 import "./Orders.css";
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
+const currencies = [
+  {
+    value: '1',
+    label: '1',
+  },
+  {
+    value: '2',
+    label: '2',
+  },
+  {
+    value: '3',
+    label: '3',
+  },
+  {
+    value: '4',
+    label: '4',
+  },
+  {
+    value: '5',
+    label: '5',
+  },
+];
+
 export default class Orders extends React.Component {
   state = {
     userskills: [], 
-    open: false
+    open: false, 
+    skills: [],
+    level: 0, 
+    language: ""
   }
   componentDidMount(){
     let id = localStorage.getItem('id');
@@ -30,6 +60,15 @@ export default class Orders extends React.Component {
               userskills: response
             })
         });
+
+    fetch('http://cloud-dev2.us-east-2.elasticbeanstalk.com/get_all_skills')
+    .then(res => res.json())
+    .catch(error => alert("Invalid User"))
+    .then(response => {
+        this.setState({
+          skills: response
+        })
+    });
   }
 
   handleDelete(e){
@@ -65,7 +104,42 @@ export default class Orders extends React.Component {
 
   handleClose(){
     this.setState({
-      open: false
+      open: false,
+      level: 0, 
+      language: ""
+    })
+  }
+
+  handleAddSkill(){
+    let data={
+      id_user: parseInt(localStorage.getItem('id')),
+	    id_skills: parseInt(this.state.language.id),
+	    level: parseInt(this.state.level)
+    }
+    if (data.id_skills && data.id_user && data.level){
+      fetch('http://cloud-dev2.us-east-2.elasticbeanstalk.com/add_user_skill', {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .catch(error => alert("Could not add skill, please try again later"))
+      .then(response => {
+          if (response){
+            window.location.reload();
+          }
+      });
+
+    } else {
+      alert("Please Fill Everything out!");
+    }
+  }
+
+  handleLevelChange(e){
+    this.setState({
+      level: e.target.value
     })
   }
 
@@ -112,8 +186,43 @@ export default class Orders extends React.Component {
         className="modal"
         >
           <div className="paper">
-            <h2 id="spring-modal-title">Spring modal</h2>
-            <p id="spring-modal-description">react-spring animates me.</p>
+            <h2 id="spring-modal-title">Add Skill</h2>
+            <Autocomplete
+              id="combo-box-demo"
+              options={this.state.skills}
+              getOptionLabel={(option) => option.programming_language}
+              style={{ width: 300 }}
+              value={this.state.language}
+              onChange={(event, newValue) => {
+                this.setState({language: newValue});
+              }}
+              renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+            />  
+            <br/>
+            <br/>
+            <TextField
+              id="standard-select-currency"
+              select
+              label="Level"
+              helperText="Please select your skill level"
+              value={this.state.level}
+              onChange={this.handleLevelChange.bind(this)}
+            >
+              {currencies.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className="submit"
+                onClick={this.handleAddSkill.bind(this)}
+            >
+              Add Skill
+            </Button>
           </div>
         </Modal>
         
